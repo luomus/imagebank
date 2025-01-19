110b4f0f94fe0fae22357257b72a08c0b1bcb538
esko-piirainen <esko.piirainen@helsinki.fi>
2025-01-19 23:08:11 +0200

    initialization server

---
+++ src/fi/laji/imagebank/endpoints/InitializationServlet.java
@@ -0,0 +1,30 @@
+package fi.laji.imagebank.endpoints;
+
+import javax.servlet.annotation.WebServlet;
+
+import fi.luomus.commons.containers.rdf.Qname;
+
+@WebServlet(name="ThreadStarterServlet", loadOnStartup=1, urlPatterns={""})
+public class InitializationServlet extends ImageBankBaseServlet {
+
+	private static final long serialVersionUID = -7554833707007928043L;
+
+	@Override
+	protected void applicationInit() {
+		Thread t = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				System.out.println(" === ImageBank STARTING UP  === ");
+				try {
+					getTaxonomyDAO().getTaxon(new Qname("MX.1"));
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+
+		});
+		t.setName("Initialization Thread");
+		t.start();
+	}
+
+}
