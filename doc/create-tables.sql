e1f53d3e3e4d6d5cb681c5a6282ddf94454d01bf
esko-piirainen <esko.piirainen@helsinki.fi>
2025-02-14 21:25:24 +0200

    fix preference max length

--- doc/create-tables.sql
+++ doc/create-tables.sql
@@ -1,6 +1,6 @@
 CREATE TABLE preferences (
   userid varchar2(10),
-  preference varchar2(20),
+  preference varchar2(200),
   value varchar2(4000),
   CONSTRAINT preferences_pk PRIMARY KEY (userid, preference)
 );
